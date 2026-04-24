a79912c62aca7c8248e108f32d69f1cb7ea30d88
Shubham Prajapati <skshivam771@gmail.com>
2026-04-24 17:20:43 +0530

    Update wsgi.py

--- mental_health_app/wsgi.py
+++ mental_health_app/wsgi.py
@@ -1,4 +1,10 @@
 import os
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mental_health_app.settings')
+
 application = get_wsgi_application()
+
+# Wrap with WhiteNoise
+application = WhiteNoise(application, root=os.path.join(os.path.dirname(__file__), '..', 'staticfiles'))
